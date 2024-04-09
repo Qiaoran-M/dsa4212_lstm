8e078fb2563e9b9c06417fd53468c94e69a8f0c3
Qiaoran-M <e0492494@u.nus.edu>
2024-04-09 20:34:25 +0800

    Fixed lstm data and modify model

--- model/lstm.py
+++ model/lstm.py
@@ -1,11 +1,11 @@
 from functools import partial
 from model.helper import *
 import jax
-from jax.experimental import optimizers
 
 
 # move forwarding and gradient computation outside to avoid jax in class object issue
-def forward(params, states, X_batch):
+@jax.jit
+def forward(params, states, X):
     Wf, bf, Wi, bi, Wc, bc, Wo, bo, Wy, by = params
     def step(prev_state, x):
         h_prev, c_prev = prev_state
@@ -18,18 +18,20 @@ def forward(params, states, X_batch):
         h = O * jnp.tanh(c)
         output = Wy @ h + by
         return (h, c), output
-    states, outputs = jax.lax.scan(step, states, X_batch[:, :, jnp.newaxis])
-    return states, outputs[:, :, 0]
+    states, outputs = jax.lax.scan(step, states, X[:, :, jnp.newaxis])
+    return states, outputs[:, 0, 0]
+forward_batch = jax.vmap(forward, in_axes=(None, 0, 0))
 
-def mse_loss(params, states, X_batch, Y_batch, l2_penalty=0.001):
-    states, Y_batch_pred = forward(params, states, X_batch)
+@jax.jit
+def mse_loss(params, states, X_batch, Y_batch):
+    states, Y_batch_pred = forward_batch(params, states, X_batch)
     squared_diff = (Y_batch_pred - Y_batch) ** 2
     err = jnp.mean(squared_diff, axis=1)  # N*1
     loss_value = jnp.mean(err)  # scalar
     # try L2 reg
-    l2_reg = 0.5 * l2_penalty * sum(jnp.sum(p**2) for p in params)
+    l2_reg = 0.5 * 0.001 * sum(jnp.sum(p**2) for p in params)
     loss_value = jnp.mean(err) + l2_reg
-    return states, loss_value
+    return loss_value, states
 
 
 class Lstm():
@@ -54,13 +56,13 @@ class Lstm():
         assert mode in ['TRAIN', 'EVAL'], f"{mode} mode is not recognized (use TRAIN / EVAL)!"
         epoch_losses = []
         # initialize state for each epoch
-        states = (jnp.zeros((self.dimensions[1], 1)), jnp.zeros((self.dimensions[1], 1)))
+        states = (jnp.zeros((self.batch_size, self.dimensions[1], 1)), jnp.zeros((self.batch_size, self.dimensions[1], 1)))
         # batch training
         for batch in range(num_batches):
             # select batch training data 
             X_batch = X[batch * self.batch_size : (batch + 1) * self.batch_size]
             Y_batch = Y[batch * self.batch_size : (batch + 1) * self.batch_size]
-            (states, loss), grads = jax.value_and_grad(mse_loss, has_aux=True)(self.params, states, X_batch, Y_batch)
+            (loss, states), grads = jax.value_and_grad(mse_loss, has_aux=True)(self.params, states, X_batch, Y_batch)
             # update parameters
             if mode == 'TRAIN':
                 self.params = [param - self.learning_rate * np.clip(grad, -1, 1) 
@@ -82,5 +84,12 @@ class Lstm():
         print('Training finished!')
     
     def predict(self, X_test):
-        _, outputs = forward(self.params, X_test)
-        return forward(self.params, X_test)
+        # initialize state for each epoch
+        states = (jnp.zeros((self.batch_size, self.dimensions[1], 1)), jnp.zeros((self.batch_size, self.dimensions[1], 1)))
+        # batch training
+        outputs = []
+        for batch in range(X_test.shape[0] // self.batch_size):
+            X_batch = X_test[batch * self.batch_size : (batch + 1) * self.batch_size]
+            states, batch_outputs = forward_batch(self.params, states, X_batch)
+            outputs.append(batch_outputs)
+        return jnp.concatenate(outputs, axis=0)
